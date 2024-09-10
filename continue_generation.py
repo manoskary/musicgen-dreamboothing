0cce72494eae7d6bb86f65ae63f26428adc6d743
melkisedeath <manoskaristineos@gmail.com>
2024-09-10 17:00:02 +0200

    Some comments to restructure the continue generation script.

--- continue_generation.py
+++ continue_generation.py
@@ -31,35 +31,51 @@ if not os.path.exists(os.path.join(base_dir, fn)):
 
 sample, sr = librosa.load(os.path.join(base_dir, fn), sr=32000)
 
-sample_1 = sample[len(sample) // 4 : ]
-sample_2 = sample[len(sample) // 4 : ]
-
-config = PeftConfig.from_pretrained(model_path)
-model = AutoModelForTextToWaveform.from_pretrained(config.base_model_name_or_path, torch_dtype=torch.float16)
-model = PeftModel.from_pretrained(model, model_path).to(device)
-
-processor = AutoProcessor.from_pretrained(model_path)
+# normalize the audio sample to [-1, 1]
+sample = sample / np.max(np.abs(sample))
 
 
-inputs = processor(
-    audio=[sample_1, sample_2],
-    sampling_rate=sr,
-    text=["sad, ambient, soundscape, piano, strings, flute", "happy, upbeat, piano, strings, flute, soundscape"],
-    padding=True,
-    return_tensors="pt"
-).to(device)
+# Input prompt:
+input_prompt = "sad ambient soundscape piano strings flute"
 
-inputs["input_values"] = inputs["input_values"].half()
+# Output prompt:
+output_prompt = "happy upbeat piano strings flute soundscape"
 
-audio_values = model.generate(**inputs, do_sample=True, guidance_scale=1, max_new_tokens=750)
-audio_values = processor.batch_decode(audio_values, padding_mask=inputs.padding_mask)
+# TODO: ~7 seconds of the source or previoussly generated audio is used as prompt, we can use this 7sec overlap to
+#  crossfade the generated audio to avoid clicks, phase shifts and volume changes after normalization.
+audios = [(sample[- len(sample) // 4 : ], sample[- len(sample) // 4 : ])]
+for i in range(10):
+    sample_1 = sample[- len(sample) // 4 : ]
+    sample_2 = sample[- len(sample) // 4 : ]
+
+    config = PeftConfig.from_pretrained(model_path)
+    model = AutoModelForTextToWaveform.from_pretrained(config.base_model_name_or_path, torch_dtype=torch.float16)
+    model = PeftModel.from_pretrained(model, model_path).to(device)
+
+    processor = AutoProcessor.from_pretrained(model_path)
+
+
+    inputs = processor(
+        audio=[sample_1, sample_2],
+        sampling_rate=sr,
+        text=["sad, ambient, soundscape, piano, strings, flute", "happy, upbeat, piano, strings, flute, soundscape"],
+        padding=True,
+        return_tensors="pt"
+    ).to(device)
+
+    inputs["input_values"] = inputs["input_values"].half()
+
+    audio_values = model.generate(**inputs, do_sample=True, guidance_scale=3, max_new_tokens=1100)
+    audio_values = processor.batch_decode(audio_values, padding_mask=inputs.padding_mask)
+    sample = audio_values[0]
+
+    audios.append((audio_values[0][-len(audio_values[0]) // 4:], audio_values[1][-len(audio_values[1]) // 4:]))
 
 # add the new audio to the original samples
-audio_file_01 = audio_file_01 = np.hstack((sample[:len(sample) // 4], audio_values[0].squeeze()))
-audio_file_02 = np.hstack((sample[:len(sample) // 2], audio_values[1].squeeze()))
+audio_file_01 = np.hstack((sample[: -len(sample) // 4], audio_values[0].squeeze()))
+audio_file_02 = np.hstack((sample[: -len(sample) // 4], audio_values[1].squeeze()))
 
 sampling_rate = model.config.audio_encoder.sampling_rate
-audio_values = audio_values.cpu().float().numpy()
 
-sf.write(os.path.join(base_dir, os.path.splitext(fn)[0] + "_0_continue.wav"), audio_file_01.T, sampling_rate)
-sf.write(os.path.join(base_dir, os.path.splitext(fn)[0] + "_1_continue.wav"), audio_file_02.T, sampling_rate)
+sf.write(os.path.join(base_dir, os.path.splitext(fn)[0] + "_0_continue.wav"), audio_file_01, sampling_rate)
+sf.write(os.path.join(base_dir, os.path.splitext(fn)[0] + "_1_continue.wav"), audio_file_02, sampling_rate)
