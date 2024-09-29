14e7d66f05ab75a5b2048ec7a43420ae4dca5592
Joann Ching <joann@sxrk09.cp.jku.at>
2024-09-29 18:56:00 +0200

    debugging for cont-gen

--- continue_generation.py
+++ continue_generation.py
@@ -1,6 +1,7 @@
 import random
 from peft import PeftConfig, PeftModel
 from transformers import AutoModelForTextToWaveform, AutoProcessor
+from collections import deque
 import torch
 import soundfile as sf
 import os
@@ -9,6 +10,7 @@ import argparse
 import numpy as np
 import pandas as pd
 import maad
+import pdb
 
 
 def parse_args():
@@ -19,7 +21,7 @@ def parse_args():
     parser = argparse.ArgumentParser()
 
     parser.add_argument("--device", type=int, default=0)
-    parser.add_argument("--model_path", type=str, default="ylacombe/musicgen-melody-punk-lora")
+    parser.add_argument("--model_path", type=str, default="jmd-musicgen-large-v11")
     parser.add_argument("--output_dir", type=str, default="artifacts")
     parser.add_argument("--guidance_scale", type=int, default=3)
     parser.add_argument("--file_name", type=str, default="musicgen_out_0.wav")
@@ -38,16 +40,17 @@ def globals_and_setup(init_state, final_state):
     This functions computes the states that need to be traversed from the init state to the final state.
 
     """
-    emotions = np.array([
-        "bored", "depressed", "sad", "upset", "stressed",
-        "nervous", "tense", "alert", "excited", "elated",
-        "happy", "contented", "serene", "relaxed", "calm"])
+    emotions = np.array(["bored", "depressed", "sad", "upset", "stressed",
+                "nervous", "tense", "alert", "excited", "elated",
+                "happy", "contented", "serene", "relaxed", "calm"])
     assert init_state in emotions
     assert final_state in emotions
-    init_index = emotions.index(init_state)
-    final_index = emotions.index(final_state)
-    assert init_index < final_index
-    return emotions[init_index, final_index]
+    init_index = np.where(emotions == init_state)[0][0]
+    final_index = np.where(emotions == final_state)[0][0]
+    # assert init_index < final_index
+    negative_emotion_threshold = np.where(emotions == "tense")[0][0]
+    assert final_index > negative_emotion_threshold
+    return emotions[init_index:final_index]  ##
 
 
 def compute_num_steps(total_time, num_states, overlap=0.25, step_time=30):
@@ -100,8 +103,9 @@ def get_prob_list_of_states(states, num_steps_per_state):
     """
     result = []
     for i, num_steps in enumerate(num_steps_per_state):
+        num_steps = round(num_steps)
         if i == 0:
-            result = [states[0]]*num_steps
+            result = [states[0]]*num_steps  ##
         else:
             for j in range(num_steps):
                 # possibility to regress to previous state by rolling the dice 1 out of 6
@@ -111,6 +115,65 @@ def get_prob_list_of_states(states, num_steps_per_state):
                 else:
                     result.append(states[i])
     return result
+
+def get_state_distribution(df: pd.DataFrame, state, mid_feature: str):
+    # Filter the DataFrame for the given size
+    filtered_df = df[df['emotion'] == state]
+    # Count the occurrences of each feature
+    feature_counts = filtered_df[mid_feature].value_counts()
+    # Calculate the total number of entries for the given feature
+    total_entries = feature_counts.sum()
+    # Calculate the percentage distribution
+    distribution = (feature_counts / total_entries).round(2)
+    # Convert to dictionary
+    return distribution.to_dict()
+
+# very hackey, not optimized, to be improved
+def sample_from_dict(dct):
+    rand_val = random.random()
+    total = 0
+    for k, v in dct.items():
+        total += v
+        if rand_val <= total:
+            return k
+    assert False, 'unreachable'
+
+#TODO: next two update functions are too similar, should be merged to one function
+def update_instrumentation(df, instruments_history, state):
+    instruments_queue = deque(instruments_history[-3:], maxlen=3)
+    next_instrumentation = instruments_queue[-1]
+    unique_instruments = len(set(instruments_queue))
+    temperature = {1: 1.0, 2: 0.5, 3: 0.3}[unique_instruments]
+    if random.random() < temperature:
+        instrument_distribution = get_state_distribution(df, state, 'instrument')
+        # avoid infinite loop of having only one instrument occurence for the emotion
+        if len(instrument_distribution) == 1:
+            next_instrumentation = next(iter(instrument_distribution))
+        else:
+            # given we want to change the instrument to another one, we need to make sure it is not the same as the previous one
+            while True:
+                next_instrumentation = sample_from_dict(instrument_distribution)
+                if next_instrumentation != instruments_history[-3]:
+                    break
+    return next_instrumentation
+
+def update_genre(df, genre_history, state):
+    genre_queue = deque(genre_history[-3:], maxlen=3)
+    next_genre = genre_queue[-1]
+    unique_genre = len(set(genre_queue))
+    temperature = {1: 1.0, 2: 0.5, 3: 0.3}[unique_genre]
+    if random.random() < temperature:
+        genre_distribution = get_state_distribution(df, state, 'genre')
+        # avoid infinite loop of having only one genre occurence for the emotion
+        if len(instrument_distribution) == 1:
+            next_instrumentation = next(iter(instrument_distribution))
+        else:
+            # given we want to change the instrument to another one, we need to make sure it is not the same as the previous one
+            while True:
+                next_genre = sample_from_dict(genre_distribution)
+                if next_genre != genre_history[-3]:
+                    break
+    return next_genre
 
 def main():
     args = parse_args()
@@ -126,7 +189,7 @@ def main():
     df = pd.read_csv(os.path.join(os.path.dirname(__file__), 'jamendo_stats.csv'), header=[0, 1])
 
     # this function needs to be here to use the df as global.
-    def compute_next_prompt(previous_prompt, next_state):
+    def compute_next_prompt(previous_prompt, next_state, instruments_history, genre_history):
         """
         given a prompt, identify instruments, mood and genre
 
@@ -146,39 +209,20 @@ def main():
             # find current genre
             if w in df["genre"].columns:
                 cur_genre.append(w)
-
-        # roll dice whether to change the instrumentation (1 to 6)
-        # TODO: find a better way to change the instrumentation
-        if random.randint(0, 6) == 1:
-            # Get from the dataset statistics all the instrumentations that had the same emotion as the next state.
-            instrument_df = df["instrument"][df["emotion"][next_state] == True]
-            # randomly pick one
-            rand_index = random.randint(0, len(instrument_df))
-            # fetch that random row
-            row = instrument_df.iloc[rand_index]
-            # find which instruments are present
-            cur_instruments = row[row != 0].keys().to_list()
-
-
-        # roll dice whether to change the genre (1 to 6):
-        # TODO: find a better way to change the genre
-        if random.randint(0, 6) == 1:
-            # Get from the dataset statistics the genre that had the same emotion as the next state.
-            genre_df = df["genre"][df["emotion"][next_state] == True]
-            # randomly pick one
-            rand_index = random.randint(0, len(genre_df))
-            # fetch that random row
-            row = genre_df.iloc[rand_index]
-            # find which genre is present
-            cur_genre = row[row != 0].keys().to_list()
+        ## cur_instruments = ['piano'], cur_genre= ['ambient']
+        # we could skip the cur variables and just access directly from the lists for the prompt
+        cur_instruments = update_instrumentation(df, instruments_history, next_state)
+        cur_genre = update_genre(df, genre_history, next_state)
+        instruments_history.append(cur_instruments)
+        genre_history.append(cur_genre)
 
         # next prompt is a combination of the next state, instrumentation (random or previous) and genre separated by comma
         # e.g. sad, piano, guitar, 80s
         prompt = [next_state] + cur_instruments + cur_genre
         prompt = ", ".join(prompt)
-        return prompt
+        return prompt, instruments_history, genre_history
 
-    # set up model staff
+    # set up model finetuning and audio parameters
     config = PeftConfig.from_pretrained(model_path)
     model = AutoModelForTextToWaveform.from_pretrained(config.base_model_name_or_path, torch_dtype=torch.float16)
     model = PeftModel.from_pretrained(model, model_path).to(device)
@@ -193,13 +237,14 @@ def main():
 
 
     audio_values = model.generate(**inputs, do_sample=True, guidance_scale=3, max_new_tokens=1503).squeeze()
+    audio_values = audio_values.cpu().detach().numpy()
     generation_length = 30 # seconds
     # 1503 tokes is 30 seconds of generation for MusicGen
     max_new_tokens = int(1503 * (1 - args.overlap))
     # Normalize the audio to keep a consistent volume level and avoid fade outs.
-    sample = audio_values / np.max(np.abs(audio_values))
+    sample = audio_values / np.max(np.abs(audio_values))  ## (960000,)
     # index_stop is used to feed the appropriate proportion of every previously generated as condition for the next
-    index_stop = int(1 / args.overlap)
+    index_stop = int(1 / args.overlap)  # 4
 
     # Compute emotion labels for the entire generation process.
     states = globals_and_setup(args.current_state, args.target_state)
@@ -209,10 +254,15 @@ def main():
 
     audios = [sample]
     text_prompt = args.input_prompt
+    instruments_history = []
+    genre_history = []
 
+    #audio_values = sample
     for i, state in enumerate(states_with_repeat):
-        sample_next = sample[- len(sample) // index_stop : ]
-        text_prompt = compute_next_prompt(text_prompt, state)
+        print('state {}/{}'.format(i, len(states_with_repeat)))
+        sample_next = sample[-len(sample) // index_stop:].flatten()  # (240000,)
+        #sample_next = audio_values[-len(audio_values) // index_stop:].flatten()
+        #text_prompt, instuments_history, genre_history = compute_next_prompt(text_prompt, state, instruments_history, genre_history)
         inputs = processor(
             audio=sample_next,
             sampling_rate=sampling_rate,
@@ -223,13 +273,19 @@ def main():
 
         inputs["input_values"] = inputs["input_values"].half()
 
-        audio_values = model.generate(**inputs, do_sample=True, guidance_scale=3, max_new_tokens=max_new_tokens).squeeze()
-        audio_values = processor.batch_decode(audio_values, padding_mask=inputs.padding_mask)
+        audio_values = model.generate(**inputs, do_sample=True, guidance_scale=3, max_new_tokens=max_new_tokens)#.squeeze()
+        audio_values = processor.batch_decode(audio_values, padding_mask=inputs.padding_mask)[0]
         sample = audio_values[0]
         # normalize sample
         sample = sample / np.max(np.abs(sample))
+        #audio_values = audio_values / np.max(np.abs(audio_values))
+        #audios.append(audio_values)  # audios[0] = (960000,)
         audios.append(sample)
 
+        if i == 20:
+            break
+
+    pdb.set_trace()
     # This utility applies cross fade for all audio segments.
     final_audio = maad.util.crossfade_list(audios, fs=sampling_rate, fade_len=generation_length*args.overlap)
     sf.write(os.path.join(base_dir, f"music_medicine_{args.current_state}-{args.target_state}.wav"), final_audio, sampling_rate)
